7a7e8de185a0fe7764d8b122ac009ed99c10c33c
Mohd Mustafa Ahmed <mustafamohdvvip@gmail.com>
2025-01-25 17:43:43 +0530

    Add files via upload

--- p2.c
+++ p2.c
@@ -1,16 +1,19 @@
 #include<stdio.h>
 void main(){
-    int num,num2;
-    printf("enter the num: ");
-    scanf("%d",&num);
-    
-    if(num%2==0){
-        printf("number is even");
-
+    int num1,num2,num3;
+    printf("enter the num1: ");
+    scanf("%d",&num1);
+    printf("enter the num1: ");
+    scanf("%d",&num2);
+    printf("enter the num1: ");
+    scanf("%d",&num3);
+    if(num1>num2 && num1>num3){
+        printf("num1 is grateer %d",num1);
     }
-    if(num%2!=0){
-        printf("number is odd");
-
+    if(num2>num1 && num2>num3){
+        printf("num2 is grateer %d",num2);
     }
-
+    else{
+         printf("num3 is grateer %d",num3);
+    }
 }
